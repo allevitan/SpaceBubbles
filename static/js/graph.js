3d8fa2d01c69109afa56be12eb15820f36c2582b
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-10-06 01:38:24 -0400

    progress

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,3 +1,34 @@
+(function ($) {
+
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+
+$.fn.waitUntilExists    = function (handler, shouldRunHandlerOnce, isChild) {
+    var found       = 'found';
+    var $this       = $(this.selector);
+    var $elements   = $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+
+    if (!isChild)
+    {
+        (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+            window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+        ;
+    }
+    else if (shouldRunHandlerOnce && $elements.length)
+    {
+        window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+    }
+
+    return $this;
+};
+
+}(jQuery));
+
 var width = 960,
     height = 500;
 
@@ -8,28 +39,9 @@ var svg = d3.select('#viz')
 
 var node, link;
 
-var voronoi = d3.geom.voronoi()
-    .x(function(d) { return d.x; })
-    .y(function(d) { return d.y; });
-    //.clipExtent([[-10, -10], [width+10, height+10]]);
-
-function recenterVoronoi(nodes) {
-    var shapes = [];
-    voronoi(nodes).forEach(function(d) {
-        if ( !d.length ) return;
-        var n = [];
-        d.forEach(function(c){
-            n.push([ c[0] - d.point.x, c[1] - d.point.y ]);
-        });
-        n.point = d.point;
-        shapes.push(n);
-    });
-    return shapes;
-}
-
 var force = d3.layout.force()
     .gravity(0.05)
-    .charge(-1000)
+    .charge(-100)
     .friction(0.3)
     .linkDistance(50)
     .size([width, height]);
@@ -42,24 +54,20 @@ force.on('tick', function() {
         .attr('y1', function(d) { return d.source.y; })
         .attr('x2', function(d) { return d.target.x; })
         .attr('y2', function(d) { return d.target.y; });
-
-    var clip = svg.selectAll('.clip')
-        .data( recenterVoronoi(node.data()), function(d) { return d.point.index; } );
-
-    clip.enter().append('clipPath')
-        .attr('id', function(d) { return 'clip-'+d.point.index; })
-        .attr('class', 'clip');
-    clip.exit().remove();
-
-    clip.selectAll('path').remove();
-    clip.append('path')
-        .attr('d', function(d) { return 'M'+d.join(',')+'Z'; });
 });
 
 d3.json('static/json/data.json', function(err, data) {
 
+    var max = 0;
+    var min = 100;
     data.nodes.forEach(function(d, i) {
         d.id = i;
+        if (d.score > max){
+            max = d.score;
+        }
+        if (d.score < min){
+            min = d.score;
+        }
     });
 
     link = svg.selectAll('.link')
@@ -67,8 +75,7 @@ d3.json('static/json/data.json', function(err, data) {
         .enter().append('line')
         .attr('class', 'link')
         .style("stroke-width", function(d) { return Math.log(d.value); })
-        .style("stroke", "black")
-        .style("stroke-opacity", 0.65);
+        .style("stroke", "black");
 
     node = svg.selectAll('.node')
         .data( data.nodes )
@@ -78,12 +85,32 @@ d3.json('static/json/data.json', function(err, data) {
         .call( force.drag );
 
     node.append('circle')
-        .attr('r', function(d){ return Math.sqrt(d.score); })
-        .attr('stroke', 'black');
+        .attr('r', function(d){
+            var value = 3.5*(((d.score - min)/(max-min))-0.5);
+            return 30*(1/(1+Math.exp(-value)));
+        })
+        .attr('fill', '#231198')
+        .attr('title', function(d) {return d.name;});
 
     force
         .nodes( data.nodes )
         .links( data.links )
         .start();
+    svg.append("p").attr('id', 'svg-finished');
 });
 
+var change = function(){
+    console.log("WAT");
+        d3.select(this).attr('r', 25)
+            .style("fill","lightcoral")
+            .style("stroke","red");
+    };
+
+$('#svg-finished').waitUntilExists(function(){
+    console.log("Sup");
+    console.log(d3.selectAll(".node")[0].length);
+    var nodes = d3.selectAll(".node")[0];
+    for (var i = 0; i<nodes.length; i++){
+        nodes[i].firstChild.on('click', change);
+    }
+});
