708791342230a8d590d02478222bbb91a17a3a3e
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-10-05 20:14:16 -0400

    Models made

--- server/models.py
+++ server/models.py
@@ -1,25 +1,28 @@
-from mongoengine import *
+from mongoengine import Document, StringField, FloatField, ImageField, ListField, ReferenceField
 
 class Page(Document):
     slug = StringField(max_length=100)
     url = StringField(max_length=255)
 
-class Site(Document):
+class Comment(Document):
+    author = ReferenceField('User')
+    site = ReferenceField('Node')
+
+class Node(Document):
     url = StringField(max_length=255)
-    #favicon = ???
+    favicon = ImageField()
     title = StringField(max_length=100)
-    score = IntField()
-    pages = ListField(ReferenceField(Page))
+    score = FloatField()
+
+    pages = ListField(ReferenceField('Page'))
+    comment = ListField(ReferenceField('Comment'))
 
 class Space(Document):
-    sites = ListField(ReferenceField(Site))
+    nodes = ListField(ReferenceField('Node'))
 
 class User(Document):
     name = StringField(max_length=50)
-    spaces = ListField(ReferenceField(Space))
-    #friends = ???
-    #Auth stuff??? Facebook?
-    
-class Comment(Document):
-    author = ReferenceField(User)
-    site = ReferenceField(Site)
+    spaces = ListField(ReferenceField('Space'))
+    friends = ListField(ReferenceField('User'))
+    password = StringField(max_length=255)
+    avatar = StringField()
